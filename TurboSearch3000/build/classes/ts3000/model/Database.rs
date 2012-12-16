5e26eaa7eb3395bd1610097c6466411cac75c6ae
Andrew Fedchuk <andrew414414@gmail.com>
2012-12-16 23:25:37 +0300

    Alpha version of GUI
    Almost done, except DocumentViewer Class and logic.
    Something is working in GUI.
    For test use test.html

---
+++ TurboSearch3000/build/classes/ts3000/model/Database.rs
@@ -0,0 +1,2 @@
+ts3000.model.SAXHandler
+ts3000.model.Database
---
+++ TurboSearch3000/src/app/AppletNumberOne.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app;
+
+/**
+ *
+ * @author Андрей
+ */
+public class AppletNumberOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
